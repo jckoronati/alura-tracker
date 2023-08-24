b7a2a216b75e9773b001104992b7ea95a8cbf0c5
Júlio Cezar Koronati <jckoronati@hotmail.com>
2023-08-24 20:45:46 -0300

    refactor: implements new interface to Alura tracker

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <SideBar />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+    <div class="column is-three-quarter">
+      <FormStylish />
+    </div>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SideBar from './components/SideBar.vue';
+import FormStylish from './components/FormStylish.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default defineComponent({
+  name: 'App',
+  components: {
+    SideBar,
+    FormStylish
   }
+});
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
